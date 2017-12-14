38eac4ceb7f23fccf33f327923ecefaa9292c621
阿銜 <a0911949761@gmail.com>
2017-12-14 15:51:28 +0800

    first commit

---
+++ resources/views/create.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <form action="{{isset($user) ? URL('users') . '/' . $user->id : ''}}"method="POST">
+                <div class="form-group">
+                    {{csrf_field()}}
+                    @if(isset($user))
+                         {{method_field('PUT')}}
+                    @endif            
+                    <input type="text" name="nama" placeholder="Nama" class="form-control"
+                    value="{{isset($user) ? $user->name :''}}">
+                    <input type="email" name="email" placeholder="Email" class="form-control"
+                    value="{{isset($user) ? $user->email :''}}">
+                    <button type="submit" class="btn btn-sm btn-success">confirm</button>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
